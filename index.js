8dc7d4eb374f0f0b0c5ee673330f649227b481db
aiswaryakv2595 <aiswaryakv2595@gmail.com>
2023-03-14 16:45:23 +0530

    database added

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const cookieParser = require("cookie-parser");
 
 const mongoose = require('mongoose');
 mongoose.set("strictQuery", false);
-mongoose.connect('mongodb://localhost:27017/oxiworld');
+mongoose.connect('mongodb+srv://aiswaryakv:aisuadoor@cluster0.w3dtvqz.mongodb.net/test');
 
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
